bd189633bfa4030b1a1e7d7393099d936497a6dc
phuoc <phuoc>
2024-04-23 16:58:58 +0700

    s

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# solar_panel
+Install Python3.10.10 [Python DL](https://www.python.org/downloads/)
+pip install fastapi[all]
+
+```bash
+ $ python -m venv venv
+ $ source venv/bin/activate or .\venv\Scripts\activate  (Windows)
+ deactivatecd
+ $ pip install -r requirements.txt
+
+docker-compose up -d --build
+```
+
+uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import os
+import numpy as np
 from ultralytics import YOLO
 
-import cv2
-model = YOLO("yolov8n.pt")
-while (1):
-    link = 0
-    results = model.predict(source=link,show=True)
-    print(results)
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+results = model.train(data="/Users/admin/Desktop/solar_panel/data.yaml", epochs=200 , batch = 16)  # train the model
